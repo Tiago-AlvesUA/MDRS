0db48630b5cfd7a977eeea586580328931dad554
Tiago-AlvesUA <tiagojba9@ua.pt>
2023-10-18 23:15:02 +0100

    1 b) code

--- task1.m
+++ task1.m
@@ -7,7 +7,7 @@ Num = 20;
 P = 1000;   %MUDAR PARA 100 000
 alfa = 0.1;
 rate = 1800;
-f = 10000;  %MUDAR PARA 1 000 000, SO PQ POR AGR É MAIS RAPIDO VER SE FUNCIONA
+f = 1000000;  
 C = [10 20 30 40];
 
 APD = zeros(4, Num);
@@ -21,10 +21,10 @@ for i = 1:4
     mAPD(i,1) = mean(APD(i,:));
     term_APD(i,1) = norminv(1-alfa/2) * sqrt(var(APD(i,:)) / Num);
 end
-fprintf('APD C10 (%%) = %.2e +- %.2e\n',mAPD(1,1),term_APD(1,1));
-fprintf('APD C20 (%%) = %.2e +- %.2e\n',mAPD(2,1),term_APD(2,1));
-fprintf('APD C30 (%%) = %.2e +- %.2e\n',mAPD(3,1),term_APD(3,1));
-fprintf('APD C40 (%%) = %.2e +- %.2e\n',mAPD(4,1),term_APD(4,1));
+fprintf('APD C10 Mbps (ms) = %.2e +- %.2e\n',mAPD(1,1),term_APD(1,1));
+fprintf('APD C20 Mbps (ms) = %.2e +- %.2e\n',mAPD(2,1),term_APD(2,1));
+fprintf('APD C30 Mbps (ms) = %.2e +- %.2e\n',mAPD(3,1),term_APD(3,1));
+fprintf('APD C40 Mbps (ms) = %.2e +- %.2e\n',mAPD(4,1),term_APD(4,1));
 
 figure(1);
 bar(C, mAPD);
@@ -41,9 +41,48 @@ ylabel('Average Packet Delay (ms)');
 title('Average Packet Delay with Error Bars');
 
 %% 1.b
+fprintf('\nB)\n');
+C = C.*10^6;
+
+perc_left = (1 - (0.19+0.23+0.17))/((109 - 64) + (1517 - 110));
+
+x = 64:1518;
+
+S = zeros(4,length(x)); % Tempo de Atendimento, S
+S2 = zeros(4,length(x)); % S^2
+APD = zeros(1,4);
+
+for j = 1:4 % Loop valores de C
+    for i = 1:length(x)
+        if x(i) == 64
+            s = (x(i) * 8) / C(j);
+            S(j,i) = 0.19 * s;
+            S2(j,i) = 0.19 * s^2;
+        elseif x(i) == 110
+            s = (x(i) * 8) / C(j);
+            S(j,i) = 0.23 * s;
+            S2(j,i) = 0.23 * s^2;        
+        elseif x(i) == 1518
+            s = (x(i) * 8) / C(j);
+            S(j,i) = 0.17 * s;
+            S2(j,i) = 0.17 * s^2;    
+        else
+            s = (x(i) * 8) / C(j);
+            S(j,i) = perc_left * s;
+            S2(j,i) = perc_left * s^2;         
+        end
+    end
+    ES = sum(S(j,:));
+    ES2 = sum(S2(j,:));
+    APD(j) = (rate * ES2)/(2*(1 - rate*ES)) + ES; % atraso médio de cada pacote no sistema
+end
+
+for i = 1:4
+    fprintf('Theoretical value APD C%d Mbps (ms) = %.2e\n',C(i)/10^6,APD(i)*1000);
+end
 
 %% 1.c) 
-fprintf('C)\n');
+fprintf('\nC)\n');
 
 C = 10;
 rates = [1000 1300 1600 1900];
